7a5fa88f543ee2a4ba14e594fadc94f03764d1fb
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-14 09:44:37 -0700

    adds messages

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const url = require('url');
 
 const server = http.createServer();
 
+let messages = [
+  { id: 1, user: 'brittany storoz', message: 'hi there!' },
+  { id: 2, user: 'bob loblaw', message: 'check out my law blog' },
+  { id: 3, user: 'lorem ipsum', message: 'dolor set amet' }
+];
+
 server.listen(3000, () => {
   console.log('The HTTP server is listening at Port 3000.');
 });
@@ -12,3 +18,5 @@ server.on('request', (request, response) => {
   response.write('Hello World\n');
   response.end();
 });
+
+
