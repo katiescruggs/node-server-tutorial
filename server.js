72a9c925caffd5a8ecaf2333f32ff2666ed1a704
katiescruggs <katie.e.scruggs@gmail.com>
2018-01-21 14:25:01 -0700

    final changes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const url = require('url');
-
 const server = http.createServer();
+
+server.listen(3000, () => {
+  console.log('The HTTP server is listening at Port 3000.');
+});
 
 let messages = [
   { id: 1, user: 'brittany storoz', message: 'hi there!' },
@@ -9,14 +12,38 @@ let messages = [
   { id: 3, user: 'lorem ipsum', message: 'dolor set amet' }
 ];
 
-server.listen(3000, () => {
-  console.log('The HTTP server is listening at Port 3000.');
-});
+function getAllMessages(response) {
+  response.writeHead(200, { 'Content-Type': 'JSON' });
+  response.write(JSON.stringify(messages));
+}
+
+function addMessage(newMessage, response) {
+  messages.push(newMessage);
+
+  console.log(newMessage);
+
+  response.writeHead(201, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(newMessage));
+  response.end();
+}
 
 server.on('request', (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello World\n');
-  response.end();
+  if (request.method === 'GET') {
+    getAllMessages(response);
+  } 
+
+  else if (request.method === 'POST') {
+    let newMessage = { 'id': new Date() };
+    
+    request.on('data', (data) => {
+      let parsedData = JSON.parse(data);
+      newMessage = Object.assign(newMessage, parsedData);
+    });
+ 
+    request.on('end', () => {
+      addMessage(newMessage, response);
+    });
+  }
 });
 
 
